d1669a2da1021ea0bb1030ee6441a77e44edf51a
caozh502 <328776293@qq.com>
2017-01-10 10:39:30 +0800

    调整了格式，添加了冰球与球门相对位置的判断

--- cjl/速度方向和坐标.cpp
+++ cjl/速度方向和坐标.cpp
@@ -13,67 +13,72 @@
 
 double get_angel(x2,y2)//获取击球手速度方向
 {
-double alpha,beta;
-double a,b,c,complex_term;
+	double alpha, beta;
+	double a, b, c, complex_term;
 
-alpha=(y3-y2)/(x3-x2);
-complex_term=pow(2*m1*v1/((m2-m1)(alpha*vx2-vy2)),2);
-a=1+complex_term;
-b=-2*complex_term*alpha;
-c=pow(alpha,2)*complex_term-1;
-beta=get_result(a,b,c);
-
-return beta;
+	alpha = (y3 - y2) / (x3 - x2);
+	complex_term = pow(2 * m1*v1 / ((m2 - m1)(alpha*vx2 - vy2)), 2);
+	a = 1 + complex_term;
+	b = -2 * complex_term*alpha;
+	c = pow(alpha, 2)*complex_term - 1;
+	beta = get_result(a, b, c);
+	
+	return beta;
 }
 double get_hitx(beta)//获取击球手击球时的x方向坐标
 {
-double sin,x1;
+	double sin, x1;
 
-cos=1/sqrt(1+pow(beta,2));
-x1=x2-(r1+r2)*cos;
-
-return x1;
+	cos = 1 / sqrt(1 + pow(beta, 2));
+	x1 = x2 - (r1 + r2)*cos;
+	return x1;
 }
 double get_hity(beta)//获取击球手击球时的y方向坐标
 {
-double cos,y1;
+	double cos, y1;
 
-sin=beta/sqrt(1+pow(beta,2));
-y1=y1-(r1+r2)*sin;
-
-return y1;
+	sin = beta / sqrt(1 + pow(beta, 2));
+	if (y2 >= y3)//判断冰球相对球门中心的y轴相对位置
+	{
+		y1 = y2 + (r1 + r2)*sin;
+	}
+	else{
+		y1 = y2 - (r1 + r2)*sin;
+	}
+		return y1;
 }
 
 double get_result(a,b,c)//求根函数
 {
-  double a,b,c;
-  t=b*b-4*a*c;
-  if(t==0) //由根的判别式来决定执行哪条分支
-  {
-    f2(a,b);
+	double a, b, c;
+	t = b*b - 4 * a*c;
+	if (t == 0) //由根的判别式来决定执行哪条分支
+	{
+		
+	  f2(a, b);
     
-  }
-  else if(t<0)
-  {
-    f1();
-  }
-  else
-  {
-    f3(a,b,c);
-  }
+	}
+	else if (t < 0)
+	{
+		f1();
+	}
+	else
+	{
+		f3(a, b, c);
+	}
 }
 void f1()
 {
-  cout<<"此方程无根！"<<endl;
+	cout << "此方程无根！" << endl;
 }
-double f2(double a,double b)
+double f2(double a, double b)
 {
-  x=-b/(2*a);
-  return x;
+	x = -b / (2 * a);
+	return x;
 }
-void f3(double a,double b,double c)
+void f3(double a, double b, double c)
 {
-  x1=((-b+(sqrt(t)))/(2*a));
-  x2=((-b-(sqrt(t)))/(2*a));
-  return max(x1,x2);
+	x1 = ((-b + (sqrt(t))) / (2 * a));
+	x2 = ((-b - (sqrt(t))) / (2 * a));
+	return max(x1, x2);
 }
